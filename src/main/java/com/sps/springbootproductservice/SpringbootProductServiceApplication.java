370493ddbd22af626cb9058372da6c62a9126446
saiprasad2k6 <saiprasad2k6@gmail.com>
2024-02-08 22:54:57 +0530

    Initial Revision

---
+++ src/main/java/com/sps/springbootproductservice/SpringbootProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sps.springbootproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sps/springbootproductservice/SpringbootProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sps.springbootproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
