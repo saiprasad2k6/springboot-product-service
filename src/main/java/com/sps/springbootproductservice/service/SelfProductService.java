505f78fc71ccdfddcbdae5363e3a6acd1734b3b0
saiprasad2k6 <saiprasad2k6@gmail.com>
2024-02-10 19:17:00 +0530

    Adding Docker compose for mysql

---
+++ src/main/java/com/sps/springbootproductservice/service/SelfProductService.java
@@ -0,0 +1,23 @@
+package com.sps.springbootproductservice.service;
+
+import com.sps.springbootproductservice.dto.GenericProductDto;
+import com.sps.springbootproductservice.exceptions.NotFoundException;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Qualifier("selfProductService")
+public class SelfProductService implements ProductService{
+    @Override
+    public List<GenericProductDto> getProductList() {
+        return null;
+    }
+
+    @Override
+    public GenericProductDto getProductById(String id) throws NotFoundException {
+        return null;
+    }
+}
