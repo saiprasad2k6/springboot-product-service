cda32bc4fdf522503504e52d06c0fc3875855e83
saiprasad2k6 <saiprasad2k6@gmail.com>
2024-02-12 09:48:19 +0530

    Adding JOined Tables with PrimaryKeyJoinColumn

--- src/main/java/com/sps/springbootproductservice/demo/joinedtable/Mentor.java
+++ src/main/java/com/sps/springbootproductservice/demo/joinedtable/Mentor.java
@@ -1,12 +1,14 @@
 package com.sps.springbootproductservice.demo.joinedtable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity(name = "jt_mentor")
 @Getter
 @Setter
+@PrimaryKeyJoinColumn(name = "user_id")
 public class Mentor extends User {
     private double averageRating;
 }
--- src/main/java/com/sps/springbootproductservice/demo/joinedtable/Student.java
+++ src/main/java/com/sps/springbootproductservice/demo/joinedtable/Student.java
@@ -1,12 +1,14 @@
 package com.sps.springbootproductservice.demo.joinedtable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity(name = "jt_student")
 @Getter
 @Setter
+@PrimaryKeyJoinColumn(name = "user_id")
 public class Student extends User {
     private double psp;
     private double attendance;
