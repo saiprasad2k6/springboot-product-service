75aebdf45ebed16bba64e086a8c43ada227107f3
saiprasad2k6 <saiprasad2k6@gmail.com>
2025-01-23 09:52:43 +0530

    Adding creation scripts

--- database/db-setup.sql
+++ database/db-setup.sql
@@ -1,12 +1,14 @@
-CREATE USER 'appuser1'@'localhost' IDENTIFIED BY 'XXX';
 drop user appuser1;
-
 drop database product_service;
-create database product_service;
-create user appuser1
-grant all privileges on product_service to appuser1;
-
 drop schema product_service
+
+create database product_service;
+
+CREATE USER 'app_user1'@'%' IDENTIFIED BY 'helloworld';
+GRANT ALL PRIVILEGES ON product_service.* TO 'app_user1'@'%' WITH GRANT OPTION;
+
+
+
 
 
 
